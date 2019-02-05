68a0b2297f534196f55b5aa3d7895e4a1f220629
Roberto Mapelli <r.mapelli@reply.it>
2019-02-05 14:00:27 +0100

    objects part

---
+++ 01/obj.js
@@ -0,0 +1,32 @@
+const point1 = {
+    x: 10,
+    y: 20,
+}
+
+const point2W = point1;
+
+point2W.y = 40;
+
+console.log(point1, point2W); // what happened ?
+
+// 1 - solution , class or function generator
+
+const pointModelGenerator = (coordinates) => ({
+    x: coordinates.x,
+    y: coordinates.y,
+});
+
+const p1 = pointModelGenerator({ x:10, y:20 });
+const p2 = pointModelGenerator({ x:10, y:40 });
+
+console.log(p1, p2); // it's ok
+
+// 2 - solution , object assign
+
+const assignedPoint = Object.assign({}, p1, { x:10, y:40 });
+
+console.log(p1, assignedPoint); // it's ok but...
+
+// 3 - solution , reduce
+
+// 4 - external libs https://lodash.com/
